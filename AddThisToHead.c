594bf324acd1fb5651fc21cab0e77afa129bbad6
TITH <1023515576@qq.com>
2022-02-28 21:31:00 +0800

    添加文件

---
+++ AddThisToHead.c
@@ -0,0 +1,55 @@
+// 单文件程序
+/**
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2022 TITH
+ * All rights reserved. 
+ * 
+ * @file     AddThisToHead.c 
+ * @brief    
+ * 
+ * @author   TITH
+ * @version  
+ * @date     2021/09/18
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+// 多文件程序
+/**
+ * This file is part of TemplateForGPL2. 
+ * Copyright (C) 2022 TITH.  
+ * All rights reserved. 
+ * 
+ * @file     AddThisToHead.c 
+ * @brief    
+ *   
+ * @author   TITH
+ * @version  
+ * @date     2021/09/18
+ * 
+ * TemplateForGPL2 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * TemplateForGPL2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with TemplateForGPL2; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
---
+++ README.md
@@ -0,0 +1,55 @@
+# TemplateForGPL2.0
+
+Copyright (C) 2022 TITH. All rights reserved. 
+
+### 介绍
+遵循 GPL 2.0 协议的项目模板
+
+### 使用说明
+1. 复制 README.md、LICENSE 文件到新的工程文件夹
+2. 修改 README.md (注意年份信息)
+3. 在工程文件源代码头部添加 AddThisToHead.c 中的内容（注意修改 Copyright 年份信息）
+
+### 编译环境
+- Windows 10
+- Visual Studio Code
+- gcc version 8.1.0 (x86_64-posix-seh-rev0, Built by MinGW-W64 project)
+
+### 作者
+TITH
+
+- github: [MirTITH (github.com)](https://github.com/MirTITH)
+- gitee: [TITHChan (tithchan) - Gitee.com](https://gitee.com/tithchan)
+
+### License
+GPL 2.0
+
+*See LICENSE file for details.*
+
+### 版本说明
+#### 1.4 
+	删除不必要信息
+
+#### 1.3
+	规范 LICENSE 文件
+
+#### 1.2  
+	修改 readme.md 文件名为 README.md  
+	更新 README.md 内容  
+
+#### 1.1  
+	修改 readme.md
+
+#### 1.0  
+	更新 copyright 行
+
+
+### 贡献者
+*（排名不分先后）*
+- TITH
+
+### 参与贡献
+1.  Fork 本仓库
+2.  新建 Feat_xxx 分支
+3.  提交代码
+4.  新建 Pull Request
